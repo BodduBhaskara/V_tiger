0e414c7d4025ec707947b265b29ce8d5c963f347
LENOVO <LENOVO@DESKTOP-ISVHKJH.mshome.net>
2026-05-13 11:04:06 +0530

    Sam login commit

---
+++ src/test/java/Login/LoginAccount.java
@@ -0,0 +1,15 @@
+package Login;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.internal.ReporterConfig;
+
+public class LoginAccount {
+	
+	@Test
+	public void login() {
+		Reporter.log("login");
+	}
+
+}
